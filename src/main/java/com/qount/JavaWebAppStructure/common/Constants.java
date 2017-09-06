bbca5555eda33bfcbfa1b035d43032e4609960ec
MateenAhmed <mateen.khan@bighalf.io>
2017-09-06 17:21:00 +0530

    code optimizations

--- src/main/java/com/qount/JavaWebAppStructure/common/Constants.java
+++ src/main/java/com/qount/JavaWebAppStructure/common/Constants.java
@@ -14,8 +14,6 @@ public class Constants {
 	public static final String UNEXPECTED_ERROR_STATUS_STR = "Un-expected Error";
 	public static final String INVALID_REQUEST_ERROR_STATUS = "Invalid Request";
 	public static final String DATABASE_ERROR_STATUS = "Database Error";
-	public static final String CASSANDRA_KEYSPACE_BIGPAY = "bigpay";
-	public static final String ADMIN_ROLE = "admin";
 	// SWAGGER VARIABLES
 	public static final String SWAGGER_API_SPEC_VERSION = "1.2.3";
 	public static final String SWAGGER_API_HTTP = "https";
