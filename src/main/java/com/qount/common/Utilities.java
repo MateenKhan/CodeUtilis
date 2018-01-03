82a0a4011ff0895d42e756d61718d6d3c7351791
MateenKhan <mateen.khan@qount.io>
2018-01-04 02:04:31 +0530

    added ltm code to utilities

--- src/main/java/com/qount/common/Utilities.java
+++ src/main/java/com/qount/common/Utilities.java
@@ -3,6 +3,8 @@ package com.qount.common;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -13,6 +15,8 @@ import org.json.JSONObject;
  */
 public class Utilities {
 
+	private static final Logger LOGGER = Logger.getLogger(Utilities.class); 
+	
 	public static final String MESSAGE = "message";
 
 	public static Response constructResponse(String message, int statusHeader) throws JSONException {
@@ -21,4 +25,22 @@ public class Utilities {
 		return Response.status(statusHeader).entity(responseJSON.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
 	}
 
+	
+	public static String getLtmUrl(String hostName, String portName) {
+		String path = null;
+		try {
+			String internalLinkingAddress = null, internalLinkingPort = null;
+			internalLinkingAddress = System.getenv(hostName);
+			internalLinkingPort = System.getenv(portName);
+			LOGGER.debug("internalLinkingAddress:"+internalLinkingAddress);
+			LOGGER.debug("internalLinkingPort:"+internalLinkingPort);
+			if (!StringUtils.isBlank(internalLinkingAddress) && !StringUtils.isBlank(internalLinkingPort)) {
+				path = "http://" + internalLinkingAddress + ":" + internalLinkingPort + "/";
+			}
+			return path;
+		} catch (Exception e) {
+			LOGGER.error(CommonUtils.getErrorStackTrace(e));
+		}
+		return null;
+	}
 }
